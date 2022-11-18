829f739fa142237c3ad5da41f7952c2c1cde2df1
Romulo <romulopiresf@gmail.com>
2022-11-17 23:48:21 -0300

    Configurando projeto

---
+++ projfinalnovostalentos/src/main/java/com/aquario/projfinalnovostalentos/ProjfinalnovostalentosApplication.java
@@ -0,0 +1,13 @@
+package com.aquario.projfinalnovostalentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjfinalnovostalentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjfinalnovostalentosApplication.class, args);
+	}
+
+}
